4b3bb107f2b2c71a24b0aa4e001770da1819fee3
flaviatschumi <tschumi97@gmail.com>
2020-09-16 17:11:53 +0200

    first commit

---
+++ uebungen/uebung_01/sketch.js
@@ -0,0 +1,136 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(120);
+
+    fill(255, 255, 255);
+
+    stroke (0)
+    strokeWeight (0.2)
+  ellipse(20,20,20,20);
+
+      stroke (0)
+    strokeWeight (0.2)
+  ellipse(60,20,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(100,20,20,20);
+
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(140,20,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(180,20,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(220,20,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(260,20,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(300,20,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(340,20,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(380,20,20,20);
+
+
+
+
+
+
+
+
+
+
+
+      stroke (0)
+    strokeWeight (0.2)
+  ellipse(20,380,20,20);
+
+      stroke (0)
+    strokeWeight (0.2)
+  ellipse(60,380,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(100,380,20,20);
+
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(140,380,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(180,380,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(220,380,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(260,380,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(300,380,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(340,380,20,20);
+
+        stroke (0)
+    strokeWeight (0.2)
+  ellipse(380,380,20,20);
+
+
+
+
+
+
+    fill(0, 0, 0);
+  rect(0, 40, 400, 320);
+
+  fill(0, 128, 128);
+  rect(0, 60, 40, 1);
+
+
+  fill(0, 128, 128);
+  rect(0, 260, 70, 1);
+
+  fill(90,180,170);
+  rect(0, 255, 20, 1);
+
+
+
+
+
+
+  fill(147,112,219);
+  rect(350, 220, 70, 1);
+
+
+  fill(199,21,133);
+  rect(360, 120, 70, 2);
+
+
+  fill(250,160,70);
+  rect(370, 118, 70, 1);
+
+}
