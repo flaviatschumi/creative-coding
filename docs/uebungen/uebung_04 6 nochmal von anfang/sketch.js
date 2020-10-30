e16c94af1e37e3fa6f96442ec798dd42ee21aaa7
flaviatschumi <tschumi97@gmail.com>
2020-10-30 19:20:28 +0100

    .

--- docs/uebungen/uebung_04 6 nochmal von anfang/sketch.js
+++ docs/uebungen/uebung_04 6 nochmal von anfang/sketch.js
@@ -1,9 +1,9 @@
 var gui;
 
 var posy = 380;
-var grosse = 400;
-var stärke = 1;
-var zeit = 1;
+var posx = 400;
+var winkel = 10;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,44 +15,49 @@ function setup() {
   noFill();
   sliderRange (0, 380, 1)
   gui = createGui('p5.gui');
-  gui.addGlobals('grosse', 'stärke', 'zeit');
+  gui.addGlobals('posy', 'winkel');
   // only call draw when then gui is changed
+  // noLoop();
+
 
 }
 
-
 function draw() {
-  // let grosse = 20;
+  // let posx = 400;
+
+
 
 
 
   //fill(c,10);
   fill('#005AE100');
   stroke(255)
-  strokeWeight(10 +(zeit));
-  for (i = 0; i < 2; i++){
+  strokeWeight(10);
+  for (i = 0; i < 2; i++)
+  // for (i = 0; i < 2; i <= anz; i++)
+   {
     push();
     translate(width / 2, height / 2);
-    rotate(frameCount / 30 + radians (stärke));
-    ellipse(0, 0, (grosse), (posy-stärke));
+    rotate(frameCount / 40 + radians (winkel));
+    ellipse(0, 0, (posx), (posy));
     pop();
 
    push();
    translate(width / 6, height / 3);
-   rotate(frameCount / 30 + radians (stärke));
-   ellipse(0, 0, (grosse), (posy-stärke));
+   rotate(frameCount / 40);
+   ellipse(0, 0, (posx), (posy));
    pop();
 
    push();
    translate(width / 1, height / 3);
-   rotate(frameCount / 30 + radians (stärke));
-   ellipse(0, 0, (grosse), (posy-stärke));
+   rotate(frameCount / 40);
+   ellipse(0, 0, (posx), (posy));
    pop();
 
    push();
    translate(width / 1.3, height / 1);
-   rotate(frameCount / 30 + radians (stärke));
-   ellipse(0, 0, (grosse), (posy-stärke));
+   rotate(frameCount / 40);
+   ellipse(0, 0, (posx), (posy));
    pop();
  }
 
@@ -61,9 +66,8 @@ function draw() {
   fill(0, 90, 225, 10);
   noStroke();
   rect(10, 10, windowWidth - 20, windowHeight - 20);
-  fill(0, 90, 225, 100);
+  // fill(0, 90, 225, 100);
   // rect((width / 2) - 50, (height / 2) - 50, 100, 100);
 
-// rotation=rotation+stärke;
 
 }
